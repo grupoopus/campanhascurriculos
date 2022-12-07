a786a0e75c244e723f7d5d1eb0c8bcf7baa53143
Itacir Gabral <itacirgabral@gmail.com>
2022-12-07 11:17:12 -0400

    initial boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practical React Query
+- https://react-query-v3.tanstack.com/guides/important-defaults
+- https://tkdodo.eu/blog/practical-react-query
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const { isLoading, error, data } = useQuery('profile', () =>
+  fetch('http://localhost:3004/profile').then(res =>
+    res.json()
+  ))
+
+  if (isLoading) return 'Loading...'
+ 
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+       <h1>{data.name}</h1>
+     </div>
+    </div>
+  )
+}
+
+export default App
