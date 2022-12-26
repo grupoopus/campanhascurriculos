1e5252310653c06eebadb7ec336c5d4011e6caab
Itacir Gabral <itacirgabral@gmail.com>
2022-12-26 13:45:59 -0400

    edit only show

--- src/pages/CampanhaEdit.jsx
+++ src/pages/CampanhaEdit.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+
+const fetchCampanhaX = ({ queryKey }) => {
+  const campanhaId = queryKey.slice(-1)
+  return fetch(`http://localhost:3004/campanhas/${campanhaId}`).then(res => res.json())
+}
 
 const CampanhaEdit = () => {
-  const { campanhaId = 0 } = useParams()
-  return <div className="flex flex-row justify-center mt-4">
-    <p>{`Edita campanha ${campanhaId}`}</p>
+  const { campanhaId } = useParams()
+  const { data: campanha } = useQuery(['campanha', campanhaId], fetchCampanhaX, { enabled: !!campanhaId })
+
+  const text = JSON.stringify(campanha, null, 2)
+  const rows = text?.split('\n').length || 3
+
+  return <div className="flex flex-col justify-center mt-4">
+    <textarea className="mx-auto md:min-w-[80%] min-w-full" rows={rows} value={text}/>
   </div>
 }
 export default CampanhaEdit
