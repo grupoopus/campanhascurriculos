a2569daaf0116fa1ef9783af6899850754ce8ae2
Itacir Gabral <itacirgabral@gmail.com>
2023-01-05 17:41:47 -0400

    all inputs

--- src/pages/Selecao.jsx
+++ src/pages/Selecao.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO']
+const cidades = ['Acrelândia', 'Assis Brasil', 'Brasiléia', 'Bujari', 'Capixaba', 'Cruzeiro do Sul', 'Epitaciolândia', 'Feijó', 'Jordão', 'Mâncio Lima', 'Manoel Urbano', 'Marechal Thaumaturgo', 'Plácido de Castro', 'Porto Acre', 'Porto Walter', 'Rio Branco', 'Rodrigues Alves', 'Santa Rosa do Purus', 'Sena Madureira', 'Senador Guiomard', 'Tarauacá', 'Xapuri']
+
 const Selecao = () => {
   return <main className='max-w-2xl mx-auto mt-16 border'>
     <form action='#' method='POST'>
@@ -48,6 +51,28 @@ const Selecao = () => {
         <div className='flex items-center mt-1'>
           <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={1300}/>
           <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={60000}/>
+        </div>
+      </div>
+      <div className=' mt-2'>
+        <p className='text-sm font-medium text-gray-700'>localidade</p>
+        <div className='flex items-center mt-1'>
+          <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
+            {
+              [
+                <option key={'estado'}>Estado</option>,
+                ...estados.map(el => <option key={el} value={el}>{el}</option>)
+              ]
+            }
+          </select>
+          <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
+          {
+              [
+                <option key={'cidades'}>Cidade</option>,
+                ...cidades.map(el => <option key={el} value={el}>{el}</option>)
+              ]
+            }
+          </select>
+          <button type='button' className='bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-1 px-2 mx-2 border-2 border-gray-500 hover:border-transparent rounded-lg'>ADICIONAR</button>
         </div>
       </div>
     </form>
