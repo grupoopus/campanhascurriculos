5fdc345ee0c16d225e98a42ed2968558707e3bf2
Itacir Gabral <itacirgabral@gmail.com>
2023-01-06 11:59:42 -0400

    selecao all fields

--- src/components/NavBarListItem.jsx
+++ src/components/NavBarListItem.jsx
@@ -6,12 +6,12 @@ const NavBarListItem = ({ children, to }) => {
   const inner = children.split(' ').map((word, idx, lst) => <p className={lst.length > 1 ? 'text-center' : null} key={idx}>{word}</p>)
 
   return <li className="mr-4">
-    <NavLink to={to} className={({ isActive }) => isActive ? 'font-bold' : null}>
+    <NavLink to={to} className={({ isActive }) => isActive ? 'font-bold' : 'font-normal'}>
       {inner}
     </NavLink>
   </li>
 }
-
+// transition ease-in-out delay-150 font-normal hover:font-semibold duration-700
 NavBarListItem.propTypes = {
   children: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired
--- src/pages/Selecao.jsx
+++ src/pages/Selecao.jsx
@@ -4,91 +4,88 @@ const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG
 const cidades = ['Acrelândia', 'Assis Brasil', 'Brasiléia', 'Bujari', 'Capixaba', 'Cruzeiro do Sul', 'Epitaciolândia', 'Feijó', 'Jordão', 'Mâncio Lima', 'Manoel Urbano', 'Marechal Thaumaturgo', 'Plácido de Castro', 'Porto Acre', 'Porto Walter', 'Rio Branco', 'Rodrigues Alves', 'Santa Rosa do Purus', 'Sena Madureira', 'Senador Guiomard', 'Tarauacá', 'Xapuri']
 
 const Selecao = () => {
-  return <main className='max-w-2xl mx-auto mt-16 border'>
-    <form action='#' method='POST'>
-      <div className=' mt-2'>
-        <label htmlFor='input_funcao' className='block text-sm font-medium text-gray-700'>Função</label>
-        <input id="input_funcao" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500' type="text"/>
+  return <form action='#' method='POST' className='max-w-2xl mx-auto mt-16 border grid gap-4 grid-cols-12'>
+    <div className='border col-span-9'>
+      <label htmlFor='input_funcao' className='block text-sm font-medium text-gray-700'>Função</label>
+      <input id="input_funcao" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500' type="text"/>
+    </div>
+    <div className='border col-span-3 flex items-center'>
+      <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' id='input-pcd' type='checkbox' />
+      <label className='ml-2 block text-sm text-gray-500' htmlFor='deficiencia'>PCD</label>
+    </div>
+    <div className='border col-span-6'>
+      <label htmlFor='select-carteira-carros' className='block text-sm font-medium text-gray-700'>CNH</label>
+      <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
+        <option >Selecione</option>
+        <option value="b">B</option>
+        <option value="c">C</option>
+        <option value="d">D</option>
+        <option value="e">E</option>
+      </select>
+    </div>
+    <div className='border col-span-3 flex items-center'>
+      <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' id='input-cnha' type='checkbox' />
+      <label className='ml-2 block text-sm text-gray-500' htmlFor='input-cnha'>CNH A (moto)</label>
+    </div>
+    <div className='border col-span-3'>
+      <p className='text-sm font-medium text-gray-700'>sexo</p>
+      <div className='flex items-center mt-1'>
+        <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="radio" name="sexo" id="sexo_masculino" value="masculino" />
+        <label className='ml-2 block text-sm text-gray-500' htmlFor="sexo_masculino">masculino</label>
       </div>
-      <div className=' mt-2'>
-        <label htmlFor='select-carteira-carros' className='block text-sm font-medium text-gray-700'>CNH</label>
-        <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
-          <option >Selecione</option>
-          <option value="b">B</option>
-          <option value="c">C</option>
-          <option value="d">D</option>
-          <option value="e">E</option>
-        </select>
+      <div className='flex items-center mt-1'>
+        <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="radio" name="sexo" id="sexo_feminino" value="feminino" />
+        <label className='ml-2 block text-sm text-gray-500' htmlFor="sexo_feminino">feminino</label>
       </div>
-      <div className='flex items-center mt-2'>
-        <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' id='input-cnha' type='checkbox' />
-        <label className='ml-2 block text-sm text-gray-500' htmlFor='input-cnha'>CNH A (moto)</label>
+      <div className='flex items-center mt-1'>
+        <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="radio" name="sexo" id="sexo_qualquer" value="sexo_qualquer" checked={true}/>
+        <label className='ml-2 block text-sm text-gray-500' htmlFor="sexo_qualquer">qualquer</label>
       </div>
-      <div className='flex items-center  mt-2'>
-        <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' id='input-pcd' type='checkbox' />
-        <label className='ml-2 block text-sm text-gray-500' htmlFor='deficiencia'>PCD</label>
+    </div>
+    <div className='border col-span-4'>
+      <p className='text-sm font-medium text-gray-700'>Idade</p>
+      <div className='flex items-center mt-1'>
+        <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500 mr-1' type="number" value={18}/>
+        <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500 ml-1' type="number" value={80}/>
       </div>
-      <div className='mt-2'>
-        <p className='text-sm font-medium text-gray-700'>sexo</p>
-        <div className='flex items-center mt-1'>
-          <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="radio" name="sexo" id="sexo_masculino" value="masculino" />
-          <label className='ml-2 block text-sm text-gray-500' htmlFor="sexo_masculino">masculino</label>
-        </div>
-        <div className='flex items-center mt-1'>
-          <input className='h-5 w-5 rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="radio" name="sexo" id="sexo_feminino" value="feminino" />
-          <label className='ml-2 block text-sm text-gray-500' htmlFor="sexo_feminino">feminino</label>
-        </div>
+    </div>
+    <div className='border col-span-8'>
+      <p className='text-sm font-medium text-gray-700'>Salário</p>
+      <div className='flex items-center mt-1'>
+        <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500 mr-1' type="number" value={1300}/>
+        <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500 ml-1' type="number" value={60000}/>
       </div>
-      <div className='mt-2'>
-        <p className='text-sm font-medium text-gray-700'>Idade</p>
-        <div className='flex items-center mt-1'>
-          <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={18}/>
-          <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={80}/>
-        </div>
-      </div>
-      <div className='mt-2'>
-        <p className='text-sm font-medium text-gray-700'>Salário</p>
-        <div className='flex items-center mt-1'>
-          <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={1300}/>
-          <input className='w-full rounded-lg border-gray-300 text-gray-500 focus:border-gray-500 focus:ring-gray-500' type="number" value={60000}/>
-        </div>
-      </div>
-      <div className=' mt-2'>
-        <p className='text-sm font-medium text-gray-700'>localidade</p>
-        <div className='flex items-center mt-1'>
-          <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
-            {
-              [
-                <option key={'estado'}>Estado</option>,
-                ...estados.map(el => <option key={el} value={el}>{el}</option>)
-              ]
-            }
-          </select>
-          <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500'>
+    </div>
+    <div className='border col-span-12'>
+      <p className='text-sm font-medium text-gray-700'>localidade</p>
+      <div className='flex items-center mt-1'>
+        <select id="select-carteira-carros" className='border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500 mr-1'>
           {
-              [
-                <option key={'cidades'}>Cidade</option>,
-                ...cidades.map(el => <option key={el} value={el}>{el}</option>)
-              ]
-            }
-          </select>
-          <button type='button' className='bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-1 px-2 mx-2 border-2 border-gray-500 hover:border-transparent rounded-lg'>ADICIONAR</button>
-        </div>
+            [
+              <option key={'estado'}>UF</option>,
+              ...estados.map(el => <option key={el} value={el}>{el}</option>)
+            ]
+          }
+        </select>
+        <select id="select-carteira-carros" className='w-full border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring-gray-500 ml-1'>
+        {
+            [
+              <option key={'cidades'}>Cidade</option>,
+              ...cidades.map(el => <option key={el} value={el}>{el}</option>)
+            ]
+          }
+        </select>
+        <button type='button' className='bg-transparent hover:bg-gray-500 text-gray-700 hover:text-white py-1 px-2 mx-2 border-2 border-gray-500 hover:border-transparent rounded-lg'>ADICIONAR</button>
       </div>
-    </form>
-  </main>
+    </div>
+    <div className='border col-span-12'>
+      <p>MT - Cuiabá</p>
+      <p>MT - Várgea Grande</p>
+      <p>MT - Santo Antônio do Leverger</p>
+    </div>
+    <div className='border col-span-12 text-center'>
+      <button type='button' className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'>Consultar</button>
+    </div>
+  </form>
 }
-// focus:border-indigo-500 focus:ring_indigo
 export default Selecao
-
-/*
-
-    <input type="radio" name="sexo" id="sexo_masculino" value="masculino" />
-    <label htmlFor="sexo_masculino">masculino</label>
-    <input type="radio" name="sexo" id="sexo_feminino" value="feminino" />
-    <label htmlFor="sexo_masculino">feminino</label>
-    <input type="number" placeholder='idade mínima'/>
-    <input type="number" placeholder='idade máxima'/>
-    <input type="number" placeholder='salário mínimo'/>
-    <input type="number" placeholder='salário máximo'/>
-*/
